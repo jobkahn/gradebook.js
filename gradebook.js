4b67711430b4d9b8825103be9e27c1fdb4d7154a
Job <119386242+jobkahn@users.noreply.github.com>
2025-07-25 21:23:45 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -7,7 +7,8 @@ function fetchGradeData() {
 }
 
 // TODO: Populate the table with grade data
-function populateGradebook(data) {
+function populateGradebook(data) 
+{
     // This function will take the fetched grade data and populate the table
     console.log("Populating gradebook with data:", data);   
 }
