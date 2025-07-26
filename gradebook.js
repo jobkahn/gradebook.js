bb6be24e2863e53a09e871fca0c8fcaee79ed93b
Job <119386242+jobkahn@users.noreply.github.com>
2025-07-25 20:23:03 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,17 @@
+// TODO: Fetch data from the PostgreSQL database (to be implemented later)
+function fetchGradeData() {
+    // This function will query the PostgreSQL database and return grade data 
+    console.log("Fetching grade data...");
+    // Placeholder return statement for demonstration purposes
+    return []; // Return an empty array or mock data for now
+}
+
+// TODO: Populate the table with grade data
+function populateGradebook(data) {
+    // This function will take the fetched grade data and populate the table
+    console.log("Populating gradebook with data:", data);   
+}
+
+// Call the stubs to demonstrate the workflow
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
